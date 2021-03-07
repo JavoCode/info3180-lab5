8cf408c3b410ceab2fbefcadeada92c700b4c0d8
Javon Ellis <javon.ellis@aye-tech.com>
2021-03-07 18:55:37 -0500

    edit env variables

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
-from .config import Config
+from app import config
 
 app = Flask(__name__)
 
@@ -12,5 +12,5 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-app.config.from_object(Config)
+app.config.from_object(config)
 from app import views
