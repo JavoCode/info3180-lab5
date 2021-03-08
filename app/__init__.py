37794c98975c0536def4ce9355adb3d333d64d2a
Javon Ellis <javon.ellis@aye-tech.com>
2021-03-07 20:24:42 -0500

    DATABASE CONNECTED

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
-from app import config
+from .config import Config
 
 app = Flask(__name__)
 
@@ -12,5 +12,5 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-app.config.from_object(config)
+app.config.from_object(Config)
 from app import views
